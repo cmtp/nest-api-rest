ddc253854909004509f5b584122f3d9fb03acd42
cmtp <christian.tola@hotmail.com>
2019-02-12 09:24:41 -0400

    add contact module

---
+++ src/contact/contact.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { ContactService } from './contact.service';
+
+@Controller('contact')
+export class ContactController {
+    constructor(private readonly contactService: ContactService) {}
+
+    @Get()
+    getContact(): string {
+        return this.contactService.getContact();
+    }
+}
